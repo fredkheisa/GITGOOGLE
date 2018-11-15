e5f1d9dc514120ea699955017d7a8dec8bd3bc33
Github User Name <you@example.com>
2018-11-15 21:32:25 +0300

    cteated the search-form easy format

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LandingPageService } from '../service/landing-page.service';
+import { Search } from '../search';
+
 
 @Component({
   selector: 'app-search-form',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  newSearch = new Search('','','',0);
 
-  constructor() { }
+  submitUser(name){
+    this.LandingPageService.getLandingPageInfo(name.value);
+    this.repositoryService.getLandingPageRepo(name.value);
+  }
+
+  constructor(private LandingPageService:LandingPageService,private repositoryService:LandingPageService) { }
 
   ngOnInit() {
   }
