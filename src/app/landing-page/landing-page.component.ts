38493c5cefd6dcc3b1b3d3f073a16b0e7e08416d
Github User Name <you@example.com>
2018-11-15 21:43:59 +0300

    done the serve and its successful

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LandingPageService } from '../service/landing-page.service';
+import { User } from '../user';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
+  providers: [ LandingPageService],
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+public username = '';
+repos:Repos;
+user:User;
+
+  constructor( private LandingService:LandingPageService, public repositoryService:LandingPageService)  { }
 
   ngOnInit() {
+    this.LandingService.getLandingPageInfo(this.username);
+    this.user = this.LandingService.user;
+
+    this.repositoryService.getLandingPageRepo(this.username);
+    this.repos = this.repositoryService.repos;
   }
 
 }
