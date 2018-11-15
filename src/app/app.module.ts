d4e776689a13a297bae0dd37fe82e421800d48ae
Github User Name <you@example.com>
2018-11-15 21:05:06 +0300

    created the repo and landing page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule
