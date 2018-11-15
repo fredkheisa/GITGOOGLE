7d8481e2f03a14b1b57eceb63527b71a6cd47edc
Github User Name <you@example.com>
2018-11-15 21:28:44 +0300

    created the search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LandingPageService } from './service/landing-page.service';
+
+
+
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DateCountPipe } from './date-count.pipe';
 import { ReposComponent } from './repos/repos.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import {FormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ReposComponent
+    DateCountPipe,
+    ReposComponent,
+    SearchFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+
   ],
-  providers: [],
+  providers: [LandingPageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
